2c1125613ec0365791a2d4d3c7d903a19add0ff1
sumit007 <sumitkumar281093@gmail.com>
2021-10-08 13:14:29 +0530

    learn listview list tile and static data

--- lib/learn_row.dart
+++ lib/learn_row.dart
@@ -24,13 +24,83 @@ class LearnRow extends StatelessWidget {
             IconButton(onPressed: () {print("");}, icon: const Icon(Icons.settings))
           ],
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.start,
+        body: ListView(
           children: [
-            IconButton(onPressed: () {print("");}, icon: const Icon(Icons.male_outlined)),
-            const Icon(Icons.image),
-            const Text("Hello Row", style: TextStyle(fontSize: 25),)
+            ListTile(
+              leading: const Icon(Icons.ac_unit_outlined),
+              title: const Text("AC"),
+              onTap: (){ print("AC");},
+            ),
+            ListTile(
+              leading: const Icon(Icons.dangerous),
+              title: const Text("dangerous"),
+              onTap: (){ print("dangerous");},
+            ),
+            ListTile(
+              leading: const Icon(Icons.male_outlined),
+              title: const Text("male_outlined"),
+              onTap: (){ print("male_outlined");},
+            ),
+            ListTile(
+              leading: const Icon(Icons.add),
+              title: const Text("add"),
+              onTap: (){ print("add");},
+            ),
+            ListTile(
+              leading: const Icon(Icons.backpack),
+              title: const Text("backpack"),
+              onTap: (){ print("backpack");},
+            ),
+            ListTile(
+              leading: const Icon(Icons.cabin),
+              title: const Text("cabin"),
+              onTap: (){ print("cabin");},
+            ),
+            ListTile(
+              leading: const Icon(Icons.deck_outlined),
+              title: const Text("deck_outlined"),
+              onTap: (){ print("deck_outlined");},
+            ),
+            ListTile(
+              leading: const Icon(Icons.expand),
+              title: const Text("expand"),
+              onTap: (){ print("expand");},
+            ),
+            ListTile(
+              leading: const Icon(Icons.face),
+              title: const Text("face"),
+              onTap: (){ print("face");},
+            ),
+            ListTile(
+              leading: const Icon(Icons.g_mobiledata),
+              title: const Text("g_mobiledata"),
+              onTap: (){ print("g_mobiledata");},
+            ),
+            ListTile(
+              leading: const Icon(Icons.h_mobiledata),
+              title: const Text("h_mobiledata"),
+              onTap: (){ print("h_mobiledata");},
+            ),
+            ListTile(
+              leading: const Icon(Icons.ice_skating),
+              title: const Text("ice_skating"),
+              onTap: (){ print("ice_skating");},
+            ),
+            ListTile(
+              leading: const Icon(Icons.adjust),
+              title: const Text("adjust"),
+              onTap: (){ print("adjust");},
+            ),
+            ListTile(
+              leading: const Icon(Icons.kayaking),
+              title: const Text("kayaking"),
+              onTap: (){ print("kayaking");},
+            ),
+            ListTile(
+              leading: const Icon(Icons.label_important),
+              title: const Text("label_important"),
+              onTap: (){ print("label_important");},
+            )
           ],
         )
     );
